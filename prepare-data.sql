ce2a61e66cf79facc6e921b6e9216186d3940a07
Michal Palenik <michal.palenik@freemap.sk>
2018-10-08 21:29:22 +0200

    include brideges/tunnels as entrances into parks

--- prepare-data.sql
+++ prepare-data.sql
@@ -1,5 +1,6 @@
 
 drop table if exists parks;
+-- '{park,cintorin,futbal}'
 create table www.parks as select osm_id, name, round(st_area(geography(way))) plocha, way,
 case when leisure='park' then 'park' when landuse in ('forest') or "natural" = 'wood' then 'forest' when landuse in ('meadow','grass','village_green') then 'grass' end as landuse
 from fresh_osm_polygon where (leisure='park' or landuse is not null or "natural" in ('wood','tree')) 
@@ -13,7 +14,10 @@ drop table if exists parks_entrances;
 create table www.parks_entrances as 
 select parks.osm_id, st_intersection(l.way, st_exteriorring(parks.way)) as way
 from parks, fresh_osm_line as l
-where l.highway is not null and st_intersects(l.way, parks.way)
+where l.highway is not null
+ and (exist(tags,'bridge') = false or tags->'bridge' in ('no'))
+ and (exist(tags,'tunnel') = false or tags->'tunnel' in ('no'))
+ and st_intersects(l.way, parks.way)
 ;
 
 
